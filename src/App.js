06ef44125f6c2a4eeaf6f2d5e4bea296f0165173
soccermind <mty2yyz@gmail.com>
2021-02-18 17:05:51 -0500

    Added todo components and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
+import TodoList from './todos/TodoList';
 import './App.css';
 
 
 const App = () => (
   <div className="App">
-    <h1>Hello, World!!!!</h1>
+    <TodoList />
   </div>
 );
 
