9da9321f68225efe07d2be8751d6fbe7fdde4525
soccermind <mty2yyz@gmail.com>
2021-02-19 16:36:49 -0500

    Added Redux flow for Mark As Completed button

--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { connect } from 'react-redux';
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
-import { removeTodo } from './actions';
+import { removeTodo, markTodoAsCompleted } from './actions';
 import './TodoList.css';
 
-const TodoList = ({ todos = [], onRemovePressed }) => (
+const TodoList = ({ todos = [], onRemovePressed, onCompletedPressed }) => (
   <div className = "list-wrapper">
-    <NewTodoForm />
-    {todos.map(todo => <TodoListItem todo={todo} onRemovePressed={onRemovePressed} />)}
+    <NewTodoForm />{todos.map(todo => 
+      <TodoListItem 
+        todo={todo}
+        onRemovePressed={onRemovePressed}
+        onCompletedPressed={onCompletedPressed} 
+      />)}
   </div>
 );
 
@@ -18,6 +22,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
   onRemovePressed: text => dispatch(removeTodo(text)),
+  onCompletedPressed: text => dispatch(markTodoAsCompleted(text)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
