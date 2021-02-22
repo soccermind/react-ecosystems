44313b7599757d749e77c80b1f8bd0c567aaddd5
soccermind <mty2yyz@gmail.com>
2021-02-22 17:29:45 -0500

    Added actions and thunks related to loading Todos - in progress, success, failure

--- src/todos/actions.js
+++ src/todos/actions.js
@@ -14,4 +14,20 @@ export const MARK_TODO_AS_COMPLETED = 'MARK_TODO_AS_COMPLETED';
 export const markTodoAsCompleted = text => ({
   type: MARK_TODO_AS_COMPLETED,
   payload: { text },
-})
+});
+
+export const LOAD_TODOS_IN_PROGRESS = 'LOAD_TODOS_IN_PROGRESS';
+export const loadTodosInProgress = () => ({
+  type: LOAD_TODOS_IN_PROGRESS,
+});
+
+export const LOAD_TODOS_SUCCESS = 'LOAD_TODOS_SUCCESS';
+export const loadTodosSuccess = todos => ({
+  type: LOAD_TODOS_SUCCESS,
+  payload: { todos },
+});
+
+export const LOAD_TODOS_FAILURE = 'LOAD_TODOS_FAILURE';
+export const loadTodosFailure = () => ({
+  type: LOAD_TODOS_FAILURE,
+});
