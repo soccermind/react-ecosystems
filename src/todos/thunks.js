05e60b9ff244c39ee46d451675cce50cabc2b863
soccermind <mty2yyz@gmail.com>
2021-02-23 16:55:16 -0500

    Added reducer to display is loading... message when loading data from server

--- src/todos/thunks.js
+++ src/todos/thunks.js
@@ -3,7 +3,7 @@ import { loadTodosInProgress, loadTodosSuccess, loadTodosFailure } from './actio
 export const loadTodos = () => async (dispatch, getState) => {
   try {
     dispatch(loadTodosInProgress());
-    const response = await fetch('http://localhost:8080/todos');
+    const response = await fetch('http://localhost:8080/todos-delay');
     const todos = await response.json();
   
     dispatch(loadTodosSuccess(todos));
