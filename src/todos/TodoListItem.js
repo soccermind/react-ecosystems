c767f9c52568e0d15792b8dcad7bb57fcb37026d
soccermind <mty2yyz@gmail.com>
2021-03-06 14:40:41 -0500

    Added tests

--- src/todos/TodoListItem.js
+++ src/todos/TodoListItem.js
@@ -10,10 +10,13 @@ const TodoItemContainer = styled.div`
   box-shadow: 0 4px 8px grey;
 `;
 
-const TodoItemContainerWithWarning = styled(TodoItemContainer)`
-  border-bottom: ${props => (new Date(props.createdAt) > new Date(Date.now() - 8640000 * 5) 
+export const getBorderStyleForDate = (startingDate, currentDate) => 
+ (startingDate > new Date(currentDate - 86400000 * 5)
     ? 'none'
-    : '2px solid red')};
+    : '4px solid red');
+
+const TodoItemContainerWithWarning = styled(TodoItemContainer)`
+  border-bottom: ${props => getBorderStyleForDate(new Date(props.createdAt), Date.now())};
 `;
 
 const ButtonsContainer = styled.div`
